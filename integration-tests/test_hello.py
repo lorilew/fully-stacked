6bb2fd7bdc4019d094340d87b62f54238e0724ac
Lauren Lewis <lorilew123@gmail.com>
2017-02-05 15:31:03 +0000

    Extracted integration test stuff to new repo. Made local settings to run sqlite locally.

--- integration-tests/test_hello.py
+++
@@ -1,8 +0,0 @@
-import requests
-from django.test import TestCase
-
-
-class ViewsTestClass(TestCase):
-    def testhello(self):
-        r = requests.get("web:8000")
-        self.assertIn("hello", r)
